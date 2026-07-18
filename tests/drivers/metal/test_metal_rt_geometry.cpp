1f74e996c18bc725a4da6bf58ad4c0a39c6db67e
josephsmithberger <josephsmithberger@gmail.com>
2026-07-18 15:03:05 -0400

    Fix Metal RT test copyright headers

--- tests/drivers/metal/test_metal_rt_geometry.cpp
+++ tests/drivers/metal/test_metal_rt_geometry.cpp
@@ -3,13 +3,14 @@
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
 /**************************************************************************/
 /* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
 /* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
 /*                                                                        */
 /* Permission is hereby granted, free of charge, to any person obtaining  */
 /* a copy of this software and associated documentation files (the        */
-/* "Software"), to deal in the Software without restriction, including  */
+/* "Software"), to deal in the Software without restriction, including    */
 /* without limitation the rights to use, copy, modify, merge, publish,    */
 /* distribute, sublicense, and/or sell copies of the Software, and to     */
 /* permit persons to whom the Software is furnished to do so, subject to  */
